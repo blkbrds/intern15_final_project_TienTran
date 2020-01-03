4010c5cb7db2faa94ec0deb0a89c603c65c9058b
Tien Tran <tien.tran2@asiantech.vn>
2020-01-03 09:11:23 +0700

    Screen Home: - Make UI List category menu - 2nd

--- FinalProject/FinalProject/View/Base/BaseTabBarController.swift
+++ FinalProject/FinalProject/View/Base/BaseTabBarController.swift
@@ -11,26 +11,20 @@ import UIKit
 final class BaseTabBarController: UITabBarController {
     enum ScreenType: Int {
         case home = 0
-        case headlines
         case following
-        case setting
 
         var title: String {
             switch self {
-            case .home: return "For you"
-            case .headlines: return "Headlines"
+            case .home: return "Headlines"
             case .following: return "Following"
-            case .setting: return "Setting"
             }
         }
     }
 
     static weak var shared: BaseTabBarController?
 
-    private var homeScreen = UINavigationController(rootViewController: TestHomeViewController())
-    private var headlinesScreen = UINavigationController(rootViewController: HomeViewController())
+    private var homeScreen = UINavigationController(rootViewController: HomeViewController())
     private var followingScreen = UINavigationController(rootViewController: FollowingViewController())
-    private var settingScreen = UINavigationController(rootViewController: SettingViewController())
 
     // MARK: - Life cycle
     override func viewDidLoad() {
@@ -42,18 +36,14 @@ final class BaseTabBarController: UITabBarController {
 
     // MARK: - Private funcs
     private func configViewController() {
-        homeScreen.tabBarItem = UITabBarItem(title: ScreenType.home.title, image: #imageLiteral(resourceName: "ic-foryou"), tag: ScreenType.home.rawValue)
         followingScreen.tabBarItem = UITabBarItem(title: ScreenType.following.title, image: #imageLiteral(resourceName: "ic-following"), tag: ScreenType.following.rawValue)
-        settingScreen.tabBarItem = UITabBarItem(title: ScreenType.setting.title, image: #imageLiteral(resourceName: "ic-setting"), tag: ScreenType.setting.rawValue)
-        headlinesScreen.tabBarItem = UITabBarItem(title: ScreenType.headlines.title, image: #imageLiteral(resourceName: "ic-headlines"), tag: ScreenType.setting.rawValue)
+        homeScreen.tabBarItem = UITabBarItem(title: ScreenType.home.title, image: #imageLiteral(resourceName: "ic-headlines"), tag: ScreenType.home.rawValue)
 
 //        var viewControllers: [UIViewController]?
 //        viewControllers?.append(homeScreen)
-//        viewControllers?.append(headlinesScreen)
 //        viewControllers?.append(followingScreen)
-//        viewControllers?.append(settingScreen)
 
-        setViewControllers([headlinesScreen, homeScreen, followingScreen, settingScreen], animated: true)
+        setViewControllers([homeScreen, followingScreen], animated: true)
     }
 
     private func configUI() {
