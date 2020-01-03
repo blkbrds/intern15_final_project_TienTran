81454fbcb550a7a9ac99019a1ec4810a8f938018
Tien Tran <tien.tran2@asiantech.vn>
2020-01-03 09:21:09 +0700

    Screen Home:  - Make UI List category menu

---
+++ FinalProject/FinalProject/AppDelegate/SceneDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  SceneDelegate.swift
+//  FinalProject
+//
+//  Created by PCI0002 on 1/3/20.
+//  Copyright © 2020 TranVanTien. All rights reserved.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        window = UIWindow(windowScene: windowScene)
+        configTabBarController()
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) { }
+
+    func sceneDidBecomeActive(_ scene: UIScene) { }
+
+    func sceneWillResignActive(_ scene: UIScene) { }
+
+    func sceneWillEnterForeground(_ scene: UIScene) { }
+
+    func sceneDidEnterBackground(_ scene: UIScene) { }
+
+    private func configTabBarController() {
+        let tabBarController = BaseTabBarController()
+        window?.rootViewController = tabBarController
+    }
+}
+
