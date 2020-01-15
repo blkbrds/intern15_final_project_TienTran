46b0c047d2a23acf01e3c615111063503966d4d4
Tien Tran <tien.tran2@asiantech.vn>
2020-01-15 13:21:22 +0700

    Commit M-15/1

---
+++ FinalProject/FinalProject/Model/API/APIManager.DownLoader.swift
@@ -0,0 +1,13 @@
+//
+//  API.DownLoader.swift
+//  FinalProject
+//
+//  Created by PCI0002 on 1/15/20.
+//  Copyright © 2020 TranVanTien. All rights reserved.
+//
+
+import Foundation
+
+extension APIManager.Downloader {
+    
+}
