0b6b2b03bcc942d3ff1928339907af8b600eb361
Tien Tran <tien.tran2@asiantech.vn>
2020-02-13 16:14:05 +0700

    Make ui search(update)

---
+++ FinalProject/FinalProject/View/Controller/SearchNewsTabBar/SearchNewsCell/SearchNewsCell.swift
@@ -0,0 +1,60 @@
+//
+//  SearchNewsCell.swift
+//  FinalProject
+//
+//  Created by TranVanTien on 2/13/20.
+//  Copyright © 2020 TranVanTien. All rights reserved.
+//
+
+import UIKit
+
+protocol SearchNewsCellDelegate: class {
+    func cell(_ cell: SearchNewsCell, needPerform action: SearchNewsCell.Action)
+}
+
+class SearchNewsCell: UICollectionViewCell {
+
+    enum Action {
+        case loadImage(indexPath: IndexPath)
+    }
+
+    @IBOutlet weak var newsImageView: UIImageView!
+    @IBOutlet weak var newsTitleLabel: UILabel!
+    @IBOutlet weak var newsSourceLabel: UILabel!
+    @IBOutlet weak var newsPublishedAtLabel: UILabel!
+
+    weak var delegate: SearchNewsCellDelegate?
+    var viewModel: SearchNewsCellViewModel? {
+        didSet {
+            updateUI()
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configUI()
+    }
+
+    private func configUI() {
+        newsImageView.clipsToBounds = true
+        newsImageView.layer.cornerRadius = 20
+        newsSourceLabel.clipsToBounds = true
+        newsSourceLabel.layer.cornerRadius = 7
+        newsPublishedAtLabel.clipsToBounds = true
+        newsPublishedAtLabel.layer.cornerRadius = 7
+    }
+
+    private func updateUI() {
+        guard let viewModel = viewModel else { return }
+        newsPublishedAtLabel.text = viewModel.news.publishedAt?.relativelyFormatted(short: false)
+        newsTitleLabel.text = viewModel.news.titleNews
+        newsSourceLabel.text = viewModel.news.source?.name ?? ""
+
+        if let dataImages = UserDefaults.standard.dictionary(forKey: "dataImages") as? DictionaryDataImage, let dataImage = dataImages[viewModel.news.urlImage ?? ""] {
+            newsImageView.image = UIImage(data: dataImage)
+        } else {
+            newsImageView.image = #imageLiteral(resourceName: "news-default")
+            delegate?.cell(self, needPerform: .loadImage(indexPath: viewModel.indexPath))
+        }
+    }
+}
---
+++ FinalProject/FinalProject/View/Controller/SearchNewsTabBar/SearchNewsViewController.swift
@@ -0,0 +1,133 @@
+//
+//  SearchNewsViewController.swift
+//  FinalProject
+//
+//  Created by TranVanTien on 2/13/20.
+//  Copyright © 2020 TranVanTien. All rights reserved.
+//
+
+import UIKit
+
+final class SearchNewsViewController: BaseViewController {
+
+    @IBOutlet private weak var searchCollectionView: UICollectionView!
+
+    var viewModel = SearchNewsViewModel()
+
+    private var resultsSearchController: UISearchController = {
+        let controller = UISearchController(searchResultsController: nil)
+        controller.obscuresBackgroundDuringPresentation = false
+        controller.hidesNavigationBarDuringPresentation = true
+        controller.searchBar.placeholder = "Search Anything..."
+        controller.searchBar.searchBarStyle = .default
+        controller.searchBar.tintColor = .black
+        controller.searchBar.sizeToFit()
+        return controller
+    }()
+
+    override func setupUI() {
+        super.setupUI()
+        title = "Search News"
+        configSearch()
+        configCollectionView()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        guard !resultsSearchController.isActive else { return }
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.5) {
+            self.resultsSearchController.searchBar.becomeFirstResponder()
+        }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        resultsSearchController.delegate = nil
+        resultsSearchController.searchBar.delegate = nil
+    }
+
+    private func configCollectionView() {
+        searchCollectionView.register(UINib(nibName: "SearchNewsCell", bundle: .main), forCellWithReuseIdentifier: "SearchNewsCell")
+        searchCollectionView.dataSource = self
+        searchCollectionView.delegate = self
+        configFlowLayout()
+    }
+
+    private func configSearch() {
+        resultsSearchController.searchResultsUpdater = self
+        navigationItem.searchController = resultsSearchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        definesPresentationContext = true
+    }
+
+    private func configFlowLayout() {
+        // auto resize item
+        if let flowLayout = searchCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.scrollDirection = .vertical
+            flowLayout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 0, right: 10)
+
+            let width = UIScreen.main.bounds.width
+            let itemWidth: CGFloat = width - 20
+            let itemHeight: CGFloat = itemWidth * 0.7
+
+            flowLayout.itemSize = CGSize(width: itemWidth, height: itemHeight)
+        }
+    }
+
+    func searchNewsApi(with query: String) {
+        viewModel.searchNews(query: query) { (done, _) in
+            if done {
+                self.searchCollectionView.reloadSections([0])
+            } else {
+                #warning("API Error")
+            }
+        }
+    }
+
+    deinit {
+        self.searchCollectionView.delegate = nil
+        self.searchCollectionView.dataSource = nil
+    }
+}
+
+extension SearchNewsViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+
+        if let searchString = searchController.searchBar.text, searchString.count > 3 {
+            viewModel.searchItems.removeAll()
+            searchNewsApi(with: searchString)
+        }
+    }
+}
+
+extension SearchNewsViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.getNumberOfListNews()
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let newsCell = collectionView.dequeueReusableCell(withReuseIdentifier: "SearchNewsCell", for: indexPath) as? SearchNewsCell else { return UICollectionViewCell() }
+        newsCell.delegate = self
+        newsCell.viewModel = viewModel.getSearchNewsCellViewModel(at: indexPath)
+        return newsCell
+    }
+}
+
+extension SearchNewsViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+}
+
+extension SearchNewsViewController: SearchNewsCellDelegate {
+    func cell(_ cell: SearchNewsCell, needPerform action: SearchNewsCell.Action) {
+        switch action {
+        case .loadImage(let indexPath):
+            viewModel.loadImage(indexPath: indexPath) { image in
+                if image != nil {
+                    self.searchCollectionView.reloadItems(at: [indexPath])
+                }
+            }
+        }
+    }
+}
